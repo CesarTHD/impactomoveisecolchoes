160a147683a52f39e88de6d65a611f5e4b941acf
César Tallys <82481963+CesarTHD@users.noreply.github.com>
2025-08-14 11:08:10 -0300

    Ajuste do pixel da meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Impacto Móveis",
   description: "Loja de móveis que atende a região de Brasília e seu entorno.",
   icons: {
-    icon: "/logo-impacto.jpg"
-  }
+    icon: "/logo-impacto.jpg",
+  },
 };
 
 export default function RootLayout({
@@ -19,97 +18,77 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
+        {/* Pré-carregamento de fontes */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Playwrite+HU:wght@100..400&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&family=Varela+Round&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Playwrite+HU:wght@100..400&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&family=Varela+Round&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
 
-        {/* <!-- Google Tag Manager --> */}
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                    (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-PH6XL662');
-                `,
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PH6XL662');
+            `,
           }}
         />
-        {/* <!-- End Google Tag Manager --> */}
+        {/* End Google Tag Manager */}
 
-        {/* <!-- Meta Pixel Code --> */}
+        {/* Meta Pixel Code */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '620452886117441');
-          fbq('track', 'PageView');
-          `,
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+                n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '620452886117441');
+              fbq('track', 'PageView');
+            `,
           }}
         />
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
               <img height="1" width="1" style="display:none"
-                  src="https://www.facebook.com/tr?id=620452886117441&ev=PageView&noscript=1"
-                />
-              `,
+              src="https://www.facebook.com/tr?id=620452886117441&ev=PageView&noscript=1"/>
+            `,
           }}
         />
-        {/* <!-- End Meta Pixel Code --> */}
-
-        {/* <!-- Meta Pixel Code --> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            <script>
-            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-            document,'script','https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '620452886117441', {
-            em: 'insert_email_variable'
-            });
-            fbq('track', 'PageView');
-            </script>
-            <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=620452886117441&ev=PageView&noscript=1"
-            /></noscript>
-          `,
-          }}
-        />
-        {/* <!-- End Meta Pixel Code --> */}
+        {/* End Meta Pixel Code */}
       </head>
 
-      <body
-        className={``}
-      >
+      <body>
+        {/* NoScript GTM */}
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH6XL662"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    `,
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH6XL662"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
           }}
         />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PH6XL662"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    `,
-          }}
-        />
-
         {children}
         <Analytics />
       </body>
