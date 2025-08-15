c157fe134a1468b6fd8c8b01ccc8da694b23f513
César Tallys <82481963+CesarTHD@users.noreply.github.com>
2025-08-15 10:04:33 -0300

    Adicionando rastreador de conversões do Meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,15 +58,15 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-                n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '620452886117441');
-              fbq('track', 'PageView');
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '620452886117441');
+          fbq('track', 'PageView');
             `,
           }}
         />
@@ -92,10 +92,10 @@ export default function RootLayout({
           }}
         />
 
-        {/* Rastreador de conversões */}
+        {/* Rastreador de conversões
         <Suspense fallback={null}>
           <PixelTracker />
-        </Suspense>
+        </Suspense> */}
 
         {children}
         <Analytics />
